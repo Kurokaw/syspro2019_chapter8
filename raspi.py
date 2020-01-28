109b488a6be1d35c4c801797ffeb690ecf1154c8
unknown <eren2710.m@gmail.com>
2020-01-28 17:04:18 +0900

    4J14 FirebaseからLED点灯

--- raspi.py
+++ raspi.py
@@ -11,7 +11,7 @@ import datetime
 import json
 import os
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/pi/syspro_firebase_secret.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/pi/syspro_firebase_secret.json'
 
 cred = credentials.Certificate('/home/pi/syspro_firebase_secret.json')
 firebase_admin.initialize_app(cred)
@@ -36,10 +36,12 @@ def on_snapshot(doc_snapshot, changes, read_time):
             print("ON")
             # ONにする処理
             GPIO.output(14, GPIO.HIGH)
+            time.sleep(1)
         elif led == "OFF":
-            print ("OFF")
+            print("ON")
             # OFFにする処理
             GPIO.output(14, GPIO.LOW)
+            time.sleep(1)
 
 
 on_ref = db.collection('led').where(u'led', u'==', u'ON')
@@ -48,6 +50,7 @@ off_ref = db.collection('led').where(u'led', u'==', u'OFF')
 # 監視を開始する
 doc_watch = on_ref.on_snapshot(on_snapshot)
 doc_watch = off_ref.on_snapshot(on_snapshot)
+
 
 # 温度湿度気圧センサから値を取得してFirestoreに送信する部分
 '''
